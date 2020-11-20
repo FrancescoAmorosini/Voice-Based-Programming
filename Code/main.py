a75344482da684792d8680122c168d2238bfd3c3
hilalosoft <hilaltaha@hotmail.com>
2020-11-20 13:05:24 +0000

    Add files via upload

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,8 @@
-
-
 from wit import Wit
 import sys
 import pyjulius3
 import queue
+from word2number import w2n
 
 #
 # # Initialize and try to connect
@@ -32,7 +31,27 @@ import queue
 #     print('Exiting...')
 #     client.disconnect()  # disconnect from julius
 
-client = Wit("JWG5NMZBJQYCCXVSRNC7K5B3YKM2XQQJ")
-with open('Recording2.wav', 'rb') as f:
-  resp = client.speech(f, {'Content-Type': 'audio/wav'})
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
+client = Wit("3OXTFKTQZFCKO3PEYBN3VYS23BDRCVRC")
+with open('..\Recording2.wav', 'rb') as f:
+    resp = client.speech(f, {'Content-Type': 'audio/wav'})
 print('Yay, got Wit.ai response: ' + str(resp))
+if resp['intents'][0]['name'] == 'DeclareVariable':
+    print("This Message is an DeclareVariable")
+if resp['intents'][0]['name'] == 'IfElseStatement':
+    print("This Message is an IfElseStatement")
+if resp['intents'][0]['name'] == 'IfStatements':
+    print("This Message is an IfStatements")
+if resp['intents'][0]['name'] == 'AddingComment':
+    print("This Message is an AddingComment")
+if resp['intents'][0]['name'] == 'IfElseStatement':
+    print("This Message is an IfElseStatement")
+
+print(w2n.word_to_num("two million three thousand nine hundred and eighty four"))
+print("This is one line of code"
+      "this is the same line so they should not be separated"
+      "/n this one is a new line i want to see if its noticed")
+print("this is a new print command")
