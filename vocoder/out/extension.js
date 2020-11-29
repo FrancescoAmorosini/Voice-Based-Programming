67d0e9a8f432762ccdad69cdf2d244964226caeb
Amorosini Francesco <f.amorosini@reply.it>
2020-11-29 11:02:26 +0100

    frontend fix

--- vocoder/out/extension.js
+++ vocoder/out/extension.js
@@ -51,8 +51,8 @@ function activate(context) {
         console.log('Activating the extension...');
         yield detectConda;
         outputChannel.appendLine('Activating vocoder...');
-        outputChannel.show();
-        //Environment setup
+        //outputChannel.show();
+        //Environment check
         exec(`${pre}check${ext}`, { cwd: path.resolve(cwd, shell) }, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
@@ -70,7 +70,9 @@ function activate(context) {
                 vscode.window.showInformationMessage('Everything is ready! Let\'s code!');
             }
             else {
+                //Display landing page
                 vscode.commands.executeCommand('markdown.showPreview', landingURI);
+                //Environment setup
                 vscode.window.withProgress({
                     location: vscode.ProgressLocation.Notification,
                     title: "We are setting up your environment, it might take a few minutes...",
@@ -93,8 +95,8 @@ function activate(context) {
                                 return;
                             }
                             resolve(`stdout: ${stdout}`);
-                            outputChannel.append(stdout.message);
-                            console.log('environment is ready!');
+                            outputChannel.appendLine('Modules successfully installed');
+                            console.log('Environment is ready!');
                             vscode.window.showInformationMessage('Everything is ready! Let\'s code!');
                         });
                     });
@@ -114,20 +116,19 @@ function activate(context) {
                         if (error) {
                             resolve(`error: ${error.message}`);
                             console.log(`error: ${error.message}`);
-                            //outputChannel.append(error.message);
+                            outputChannel.append(error.message);
                             vscode.window.showErrorMessage('Recording failed');
                             return;
                         }
                         if (stderr) {
                             resolve(`stderr: ${stderr}`);
                             console.log(`stderr: ${stderr}`);
-                            //outputChannel.append(stderr.message);
+                            outputChannel.append(stderr.message);
                             vscode.window.showErrorMessage('Recording failed');
                             return;
                         }
                         console.log(`stdout: ${stdout}`);
                         resolve(`stdout: ${stdout}`);
-                        //outputChannel.show();
                         //writeOnEditor(stdout); //to be removed
                         elaborateCommand();
                     });
@@ -151,33 +152,60 @@ function elaborateCommand() {
             return;
         }
         console.log(`stdout: ${stdout}`);
-        if (stdout.startsWith("vocoder-undo")) { // discuss with backend to agree on naming
+        var sections = waitforOut(stdout);
+        const vocoderSec = sections[1];
+        if (vocoderSec.includes("vocoder-undo")) {
             vscode.commands.executeCommand("undo");
             return;
         }
-        if (stdout.startsWith("vocoder-delete")) { // discuss with backend to agree on naming
+        if (vocoderSec.includes("vocoder-delete")) {
             writeOnEditor('');
             return;
         }
-        writeOnEditor(stdout);
+        const codeSec = vocoderSec.split("vocoder-code-block");
+        if (codeSec.length !== 2) {
+            console.log(`Bad format from backend processing: no command or code-block section found`);
+            vscode.window.showErrorMessage('Code processing failed');
+            return;
+        }
+        writeOnEditor(codeSec[1]);
     });
 }
 function writeOnEditor(s) {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-        vscode.window.showWarningMessage('No editor available to write on');
+    return __awaiter(this, void 0, void 0, function* () {
+        s = s.substring(s.indexOf('\r\n') + 2, s.lastIndexOf('\r\n'));
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showWarningMessage('No editor available to write on');
+            return;
+        }
+        const currSel = editor.selection;
+        yield editor.edit((edit) => { edit.replace(currSel, s); });
+        // computation of new position of the cursor
+        // line from which the selection starts: does not depend on which direction the sel is made (start>end)
+        const currLine = currSel.start.line;
+        const writtenLines = s.split(/\r\n|\r|\n/).length;
+        // create a position where to put the cursor: at the end of what just written
+        const newEnd = new vscode.Position(currLine + writtenLines - 1, 0);
+        // set the selection to an empty one where defined
+        editor.selection = new vscode.Selection(newEnd, newEnd);
+    });
+}
+let retries = 0;
+function waitforOut(output) {
+    if (!output.includes('dsd-section') && retries < 15) {
+        ++retries;
+        setTimeout(waitforOut, 100, [output, retries]);
         return;
     }
-    const currSel = editor.selection;
-    editor.edit((edit) => { edit.replace(currSel, s); });
-    // computation of new position of the cursor
-    // line from which the selection starts: does not depend on which direction the sel is made (start>end)
-    const currLine = currSel.start.line;
-    const writtenLines = s.split(/\r\n|\r|\n/).length;
-    // create a position where to put the cursor: at the end of what just written
-    const newEnd = new vscode.Position(currLine + writtenLines - 1, 0);
-    // set the selection to an empty one where defined
-    editor.selection = new vscode.Selection(newEnd, newEnd);
+    else if (retries >= 15) {
+        retries = 0;
+        console.log(`Bad format from backend processing: no dsd-section found or more than one found`);
+        vscode.window.showErrorMessage('Code processing failed');
+        return;
+    }
+    return output.split("dsd-section");
+    //real action
 }
 // this method is called when your extension is deactivated
 function deactivate() { }
